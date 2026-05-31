943aadb52d7cd90d49a12008397636b1bc15da1b
LeilaYaz <leilayazminlepiane31@gmail.com>
2026-05-31 00:59:38 -0300

    Update contador.cpp

--- contador.cpp
+++ contador.cpp
@@ -1,1 +1,27 @@
+#include "contador.h"
+#include <mutex>
+
+int tareasFinalizadas = 0;
+
+std::mutex mtx_contador;
+
+void incrementarFinalizados()
+{
+    mtx_contador.lock();
+
+    tareasFinalizadas++;
+
+    mtx_contador.unlock();
+}
+
+int obtenerFinalizados()
+{
+    mtx_contador.lock();
+
+    int valor = tareasFinalizadas;
+
+    mtx_contador.unlock();
+
+    return valor;
+}
 
